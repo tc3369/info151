fef39fd77caadcd89bf9d588eb755aa1c5890f64
tc3369 <146146567+tc3369@users.noreply.github.com>
2023-11-26 21:51:39 -0500

    sign in and burger x

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,60 @@
-// alert("hello!")
 
 var mainnav = document.querySelector("#mainnav");
-// mainnav.classList.add("showMainNav");
 
 console.log(mainnav);
 
 var burgerBtn = document.querySelector("#burger-container button");
-console.log(burgerBtn);
+// console.log(burgerBtn);
+var burgerBars = document.querySelector("#burger-bars");
+var burgerX = document.querySelector("#burger-x");
+
 
 var canYouSeeMainNav = false;
 
 // console.log("hello");
 
 function showNav(){
-    console.log("button clicked")
+    // console.log("button clicked")
+
     if(canYouSeeMainNav === false){
-        mainnav.classList.remove("showMainNav");
-        canYouSeeMainNav = true;
-    }else{
         mainnav.classList.add("showMainNav");
+        canYouSeeMainNav = true;
+
+        burgerBars.classList.add("hide-burger-bars");
+        burgerX.classList.remove("hide-burger-x");
+
+    }else{
+        mainnav.classList.remove("showMainNav");
         canYouSeeMainNav = false;
+        
+        burgerBars.classList.remove("hide-burger-bars");
+        burgerX.classList.add("hide-burger-x");
     }
 }
 burgerBtn.addEventListener("click", showNav)
+
+// ----------------------
+//      Sign In
+// ----------------------
+
+var signInBtn = document.querySelector(".signin");
+var signInContainer = document.querySelector("#signin-container");
+var closeSignInModalButton = document.querySelector("#close-sign-in-modal-button");
+
+
+
+function showSignInModal() {
+    signInContainer.classList.add("show-signin-container")
+    mainnav.classList.remove("showMainNav");
+
+}
+
+function hideSignInModal(){
+    signInContainer.classList.remove("show-signin-container")
+    
+}
+
+
+signInBtn.addEventListener("click", showSignInModal);
+closeSignInModalButton.addEventListener("click", hideSignInModal);
 
